627705738510b7e7fc21706a93ad7ee8260f178e
James Jorissen <jamesjorissen@gmail.com>
2021-07-29 14:27:40 -0500

    got routes working, added new required nodejs things

---
+++ Workout Tracker/routes/htmlroutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/excercise.html"));
+});
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+
+
+
+module.exports = router;
+
+
