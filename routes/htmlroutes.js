1416edd32e674c455fcdfa08e7ca072980bea020
James Jorissen <jamesjorissen@gmail.com>
2021-07-29 18:41:20 -0500

    one more time

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-router.get("/exercises", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/excercise.html"));
-});
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/exercises", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/excercise.html"));
+});
+
 router.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
