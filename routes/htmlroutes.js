672c54efb16c68e5432d290388372427a8b68f66
James Jorissen <jamesjorissen@gmail.com>
2021-07-29 18:59:16 -0500

    exercise debacle

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,7 +6,7 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/exercises", function (req, res) {
+router.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/excercise.html"));
 });
 
