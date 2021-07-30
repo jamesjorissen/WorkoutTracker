19014ceb987bd67623339b9791d11df88f5ecf3c
James Jorissen <jamesjorissen@gmail.com>
2021-07-29 23:03:43 -0500

    sure why not

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 const express = require("express");
+const morgan = require("morgan")
 const mongoose = require("mongoose");
-const apiRoutes = require("./routes/APIroutes");
-const htmlRoutes = require("./routes/htmlroutes")
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -12,15 +12,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 });
+
+require("./routes/APIroutes")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on this port: ${PORT}!`);
